ac208be2e87bc2202d894da517da41ac21935d87
Lydia <frankenshteynle@gmail.com>
2023-12-11 17:05:18 +0300

    add task's 2,3

---
+++ Task2.cs
@@ -0,0 +1,39 @@
+﻿//Напишите программу вычисления функции Аккермана
+//Даны два неотрицательных числа m и n. 
+//Использовать рекурсию
+
+//Буду считать, что просят
+// ввод целых чисел с консоли
+// я не поняла что такое функция Аккермана, поэтому правильность решения проверить не смогла
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите два неотрицательных числа m и n:");
+        int m = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+
+        int result = AckermannFunction(m, n);
+
+        Console.WriteLine($"Результат функции Аккермана для m = {m} и n = {n}: {result}");
+    }
+
+    static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return AckermannFunction(m - 1, 1);
+        }
+        else
+        {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+    }
+}
---
+++ Task3.cs
@@ -0,0 +1,53 @@
+﻿//Задайте произвольный массив. 
+//Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+
+//Буду считать, что просят
+// ввод целых чисел с консоли
+// я не поняла что такое функция Аккермана, поэтому правильность решения проверить не смогла
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите размер массива:");
+        int size = int.Parse(Console.ReadLine());
+        int[] array = GenerateRandomArray(size);
+        Console.WriteLine("Изначальный массив:");
+        PrintArray(array, 0);
+        Console.WriteLine("\nМассив элементов, начиная с конца:");
+        PrintArrayReversed(array, array.Length - 1);
+    }
+
+    static int[] GenerateRandomArray(int size)
+    {
+        Random random = new Random();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(1, 100);
+        }
+
+        return array;
+    }
+
+    static void PrintArray(int[] array, int index)
+    {
+        if (index < array.Length)
+        {
+            Console.Write(array[index] + " ");
+            PrintArray(array, index + 1);
+        }
+    }
+
+    static void PrintArrayReversed(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(array[index] + " ");
+            PrintArrayReversed(array, index - 1);
+        }
+    }
+}
